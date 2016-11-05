a82212380c31e139a96bbf157adfa882c2643b69
YingXiong361 <yingxiong361@gmail.com>
2016-11-05 23:37:07 +0100

    Answer to the exercise 1.3.37

---
+++ BagsQueuesStacks/JosephusProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagsQueuesStacks
+{
+    /// <summary>
+    /// Exercise 1.3.37
+    /// </summary>
+    public class JosephusProblem
+    {
+        int N;
+        int M;
+
+        public JosephusProblem(int n, int m)
+        {
+            N = n;
+            M = m;
+        }
+
+        public void Play()
+        {
+            var players = new Queue<int>();
+            for (int i = 0; i < N; i++)
+            {
+                players.Enqueue(i);
+            }
+            Eliminate(players);
+        }
+        private void Eliminate(Queue<int> persons)
+        {
+            int j = 1;
+            while (persons.Count > 1)
+            {
+                if (j % M == 0) { Console.WriteLine(persons.Dequeue()); }
+                else { persons.Enqueue(persons.Dequeue()); }
+                j++;
+            }
+            Console.WriteLine(persons.Dequeue());
+        }
+    }
+}
