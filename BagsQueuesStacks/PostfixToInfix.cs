cf96e669996247dc07f15d8c6e6c77b58df7ef49
YingXiong361 <yingxiong361@gmail.com>
2016-11-20 15:16:23 +0100

    Answer to the exercise 1.3.9

---
+++ BagsQueuesStacks/PostfixToInfix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagsQueuesStacks
+{
+    /// <summary>
+    /// Exercise 1.3.9
+    /// </summary>
+    public class PostfixToInfix
+    {
+        public const string Input = "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )";
+        public const string ExpectedOutput = "( ( 1 + 2 ) * ( ( 3 - 4 ) * (5 - 6 ) )";
+        public const string RightParentheses = ")";
+        public const string LeftParentheses = "(";
+
+        public static string Transform(string input)
+        {
+            var inputs = new Stack<string>();
+            foreach (var elem in input.Split(' '))
+            {
+                if (elem != RightParentheses)
+                {
+                    inputs.Push(elem);
+                }
+                else
+                {
+                    var input1 = inputs.Pop();
+                    var input2 = inputs.Pop();
+                    var input3 = inputs.Pop();
+                    inputs.Push(LeftParentheses + input3 + input2 + input1 + RightParentheses);
+                }
+            }
+            return inputs.Pop();
+        }
+    }
+}
