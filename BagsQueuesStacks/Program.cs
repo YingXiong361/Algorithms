3a50f11ef3a201d4e08beca10f7123cf066abea0
YingXiong361 <yingxiong361@gmail.com>
2019-08-17 14:48:08 +0200

    new project + Solution for NumSquares

--- BagsQueuesStacks/Program.cs
+++ BagsQueuesStacks/Program.cs
@@ -12,9 +12,9 @@ namespace BagsQueuesStacks
         {
             // FixedCapacityStackOfStrings.RunClient(FixedCapacityStackOfStrings.TestSample);
             // FixedCapacityStack<string>.RunClient(FixedCapacityStack<string>.TestSample);
-           // ResizableStack<string>.RunClient(ResizableStack<string>.TestSample);
+            // ResizableStack<string>.RunClient(ResizableStack<string>.TestSample);
 
-            //var queue = new ResizableQueue<string>(100);
+            //var queue = new ResizableQueue<string>(3);
             //foreach (var item in ResizableStack<string>.TestSample.Split(' ').ToList())
             //{
             //    queue.Enqueue(item.ToString());
@@ -25,7 +25,7 @@ namespace BagsQueuesStacks
             //    Console.Write(item + " ");
             //}
 
-          //  ResizableQueue<string>.RunClient(ResizableQueue<string>.TestSample);
+            // ResizableQueue<string>.RunClient(ResizableQueue<string>.TestSample);
             //StackImplementedByLinkedList<string>.RunClient(StackImplementedByLinkedList<string>.TestSample);
 
             //var stack = new StackImplementedByLinkedList<string>();
@@ -39,21 +39,100 @@ namespace BagsQueuesStacks
             //    Console.Write(item + " ");
             //}
 
-            QueueImplementedByLinkedList<string>.RunClient(QueueImplementedByLinkedList<string>.TestSample);
+            //     QueueImplementedByCircularLinkedList<string>.RunClient(QueueImplementedByCircularLinkedList<string>.TestSample);
 
-            var queue = new QueueImplementedByLinkedList<string>();
+            //var queue = new QueueImplementedByLinkedList<string>();
 
-            foreach (var item in QueueImplementedByLinkedList<string>.TestSample.Split(' ').ToList())
-            {
-                queue.Enqueue(item.ToString());
-            }
+            //foreach (var item in QueueImplementedByLinkedList<string>.TestSample.Split(' ').ToList())
+            //{
+            //    queue.Enqueue(item.ToString());
+            //}
 
-            foreach (var item in queue)
-            {
-                Console.Write(item + " ");
-            }
+            //foreach (var item in queue)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //var moveToFront = new MoveToFront();
+            //var testSample = "LBJLBJ";
+            //Console.WriteLine("Input: {0}", testSample);
+            //foreach (var item in testSample.ToCharArray())
+            //{
+            //    moveToFront.InsertInTheFront(item);
+            //}
+            //Console.WriteLine(moveToFront.ToString());
 
+            //IRingBuffer<int> buffer = new RingBuffer<int>(4);
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    buffer.Enqueue(i);
+            //    Console.WriteLine("IsFull="+buffer.IsFull());
+            //}
+
+            //RingBuffer<int>.RunClient();
+
+            //Console.WriteLine(StackGenerabilityTester.CanPermutationBeGenerated(StackGenerabilityTester.GoodSample));
+            //Console.WriteLine(StackGenerabilityTester.CanPermutationBeGenerated(StackGenerabilityTester.BadSample));
+
+            //    Buffer.TestClient();
+            //var game = new JosephusProblem(7, 2);
+            //game.Play();
+
+            //Console.WriteLine("Press any key to stop the program");
+
+            // Console.WriteLine(EvaluatePostfix.Evaluate(InfixToPostfix.Transform(InfixToPostfix.Input)));
+
+            //var testData = new int[] { 5, 1, 2, 3, 4 };
+
+            //Search.InsertionSort(testData);
+            //testData.Print();
+            Console.WriteLine(Solution.NumSquares(7168));
             Console.ReadKey();
+        }
+
+    }
+
+    public class Solution
+    {
+        public static int NumSquares(int n)
+        {
+            var pS = new List<int>();
+            for (int i = n; i >0; i--)
+            {
+                if (IsSquare(i))
+                {
+                    pS.Add(i);
+                }
+            }
+
+            return GetStep(pS,0,0,n);
+        }
+
+        private static int GetStep(List<int> ps,int sum,int? step,int n)
+        {
+            if(sum==n) return step.Value;
+            if(sum>n) return int.MaxValue;
+             step=step.Value+1;
+             var min=int.MaxValue;
+            for(int i=0;i<ps.Count;i++)
+            {
+                var x = GetStep(ps, (sum + ps[i]), step, n);
+                if(x<min)
+                {
+                    min=x;
+                }
+            }
+
+            return min;
+        }
+
+        private static bool IsSquare(int num)
+        {
+            for (int i = 1; num > 0; i += 2)
+            {
+                num -= i;
+            }
+
+            return num == 0;
         }
     }
 }
---
+++ BagsQueuesStacks/Search.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagsQueuesStacks
+{
+    public static class Search
+    {
+        public static void BubbleSort0(int[] lst)
+        {
+            for (int i = 0; i < lst.Length - 1; i++)
+            {
+                for (int j = i + 1; j < lst.Length; j++)
+                {
+                    if (lst[j] < lst[i])
+                    {
+                        lst.Switch(i, j);
+                    }
+                }
+            }
+        }
+
+        //两两比较 相邻记录 的关键字，反序则交换
+        //stable
+        public static void BubbleSort1(int[] lst)
+        {
+            for (int i = 0; i < lst.Length - 1; i++)
+            {
+                bool noSwitch = true;
+                for (int j = lst.Length - 2; j >= i; j--)
+                {
+                    if (lst[j] > lst[j + 1])
+                    {
+                        lst.Switch(j, j + 1);
+                        noSwitch = false;
+                    }
+                }
+
+                Console.WriteLine("Iterate {0}", i);
+                if (noSwitch)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] lst)
+        {
+            for (int i = 0; i < lst.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < lst.Length; j++)
+                {
+                    if (lst[j] < lst[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (i != minIndex)
+                {
+                    lst.Switch(i, minIndex);
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] lst)
+        {
+            for (int i = 1; i < lst.Length; i++)
+            {
+                int toBeInsertValue = lst[i];
+                int j;
+                for ( j = i - 1; j >= 0 && lst[j] > toBeInsertValue; j--)
+                {
+
+                    lst[j + 1] = lst[j];
+                }
+                lst[j + 1] = toBeInsertValue;
+            }
+
+        }
+
+        #region Extension
+
+        private static void Switch(this int[] lst, int i, int j)
+        {
+            var temp = lst[i];
+            lst[i] = lst[j];
+            lst[j] = temp;
+        }
+
+        public static void Print(this int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+
+        #endregion
+    }
+}
